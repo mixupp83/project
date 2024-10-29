c174e2ff31d2bc5a250fa2ae4c4473c3fda13ed0
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-10-29 12:56:46 +0400

    Update main.py
    Добавление функции вычисления максимального и минимального значения цены закрытия и сравнение разниц с заданным порогом

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import data_download as dd
 import data_plotting as dplt
 
-
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
     print(
@@ -9,8 +8,9 @@ def main():
     print(
         "Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): ")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    threshold = float(input("Введите порог колебаний в процентах (например, 10 для 10%): "))
 
     # Fetch stock data
     stock_data = dd.fetch_stock_data(ticker, period)
@@ -18,13 +18,14 @@ def main():
     # Add moving average to the data
     stock_data = dd.add_moving_average(stock_data)
 
-    # Plot the data
-    dplt.create_and_save_plot(stock_data, ticker, period)
-
     # Calculate and display average price
     dd.calculate_and_display_average_price(stock_data)
 
+    # Notify if strong fluctuations
+    dd.notify_if_strong_fluctuations(stock_data, threshold)
 
+    # Plot the data
+    dplt.create_and_save_plot(stock_data, ticker, period)
 
 if __name__ == "__main__":
     main()
